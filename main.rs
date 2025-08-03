13292b619b5e7962ecfee5666e84aae7b638297f
wetwet666 <fidhirhdvyh@protonmail.com>
2025-08-03 02:19:01 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -16,12 +16,12 @@ async fn hello_world() -> &'static str {
 
 async fn setup_environment() { 
     let env_vars = [
-        ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
+        ("UUID", "60985612-9e9e-4704-810f-652384ffe737"),
         ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("ARGO_DOMAIN", "shuttle6.sdhtyhybrh.tk"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", ""eyJhIjoiNGVmNzA1Yjc2ZGUyYjQ5MDQ1NWE2YzQ2Njg3OTZjNjMiLCJ0IjoiZGQzNzhkYzItOTA0MC00ZmMxLWE1MDgtOTBiMzc3YTcyNmVlIiwicyI6Ik16UmpOemRqWldZdE1UYzNNQzAwTWpjMkxXRXhaVGN0TW1GaFpUZzNOR0ppTnpRMyJ9),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
         ("CFIP", "time.is"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
@@ -195,7 +195,7 @@ ingress:
                     }
                 },
                 "sniffing": {
-                    "enabled": true,
+                    "enabled": false,
                     "destOverride": ["http", "tls", "quic"],
                     "metadataOnly": false
                 }
@@ -214,7 +214,7 @@ ingress:
                     }
                 },
                 "sniffing": {
-                    "enabled": true,
+                    "enabled": false,
                     "destOverride": ["http", "tls", "quic"],
                     "metadataOnly": false
                 }
@@ -234,7 +234,7 @@ ingress:
                     }
                 },
                 "sniffing": {
-                    "enabled": true,
+                    "enabled": false,
                     "destOverride": ["http", "tls", "quic"],
                     "metadataOnly": false
                 }
